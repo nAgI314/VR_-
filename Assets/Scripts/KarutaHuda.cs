5dead997ddccf8fc80f49f5b8fe56cccd37ecaf8
nAgI314 <kurosawasyunnagi@gmail.com>
2022-07-06 03:12:31 +0900

    かるたをシャッフルするスクリプトの途中

---
+++ Assets/Scripts/KarutaHuda.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarutaHuda : MonoBehaviour
+{
+    List<KarutaHuda> _KarutaList =new List<KarutaHuda>();
+    int count = 0;
+    [SerializeField] KarutaHuda _KarutaHudaPrehub = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int count = 0; count < 44; count++) ;
+        {
+            if (count == 0)
+            {
+                for ()
+                {
+
+                }
+            }
+            else if (count == 8||count==15)
+            {
+                for ()
+                {
+
+                }
+            }
+            else if (count == 22 || count == 29)
+            {
+                for ()
+                {
+
+                }
+            }
+            else if (count == 36)
+            {
+                for ()
+                {
+
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
